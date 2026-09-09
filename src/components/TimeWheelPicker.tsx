863a2c803d8af15c46e7bdc9ddd39ee588bf69f0
HoweiAY <howeiauyeung@gmail.com>
2026-09-09 17:48:18 +0800

    fix: localize time picker periods

--- src/components/TimeWheelPicker.tsx
+++ src/components/TimeWheelPicker.tsx
@@ -164,7 +164,7 @@ export function TimeWheelPicker({
         accessibilityLabel={t("editor.minute")}
       />
       <Wheel
-        data={[...PERIODS]}
+        data={[t("editor.am"), t("editor.pm")]}
         index={period === "AM" ? 0 : 1}
         width={56}
         onIndexChange={handlePeriod}
